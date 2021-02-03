4ae4f7c22aef39e5e16a462c509d69abf5e6815a
whjdarking <wang.haojian421@gmail.com>
2021-02-04 03:44:43 +0900

    ttT

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
-        proxy: 'http://back-service'
+        proxy: '47.242.143.52'//这样只在kubernetes起作用，back-service是后端的dns
+        // proxy: 'localhost:3000'
     }
 }
-//这样只在kubernetes起作用，back-service是后端的dns
